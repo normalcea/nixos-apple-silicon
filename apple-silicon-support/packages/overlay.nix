84f3700320e3e27f849f71057c234ad487fff328
Jasi <normalc@posteo.net>
2025-08-02 13:35:07 -0400

    asahi-fwextract: remove; use upstream nixpkgs

--- apple-silicon-support/packages/overlay.nix
+++ apple-silicon-support/packages/overlay.nix
@@ -2,6 +2,5 @@ final: prev: {
   linux-asahi = final.callPackage ./linux-asahi { };
   m1n1 = final.callPackage ./m1n1 { };
   uboot-asahi = final.callPackage ./uboot-asahi { };
-  asahi-fwextract = final.callPackage ./asahi-fwextract { };
   mesa-asahi-edge = final.callPackage ./mesa-asahi-edge { };
 }
