8579d8169e69b6338602086cafb96c6942574120
Jasi <normalc@posteo.net>
2025-08-11 14:09:01 -0400

    alsa-ucm-conf-asahi: use upstream

--- apple-silicon-support/packages/overlay.nix
+++ apple-silicon-support/packages/overlay.nix
@@ -4,6 +4,5 @@ final: prev: {
   uboot-asahi = final.callPackage ./uboot-asahi { };
   asahi-fwextract = final.callPackage ./asahi-fwextract { };
   mesa-asahi-edge = final.callPackage ./mesa-asahi-edge { };
-  alsa-ucm-conf-asahi = final.callPackage ./alsa-ucm-conf-asahi { inherit (prev) alsa-ucm-conf; };
   asahi-audio = final.callPackage ./asahi-audio { };
 }
