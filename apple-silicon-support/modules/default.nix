e719e260216bc99d73b030b23f3d6410f8e57260
Jasi <jawsi@posteo.net>
2025-12-09 15:08:06 -0500

    modules/drirc: add `webSecurityCompat` option for binaries

--- apple-silicon-support/modules/default.nix
+++ apple-silicon-support/modules/default.nix
@@ -38,7 +38,64 @@
           for more info.'' pkgs.mesa
       );
 
-      environment.etc."drirc".source = ./10-asahi-browser-apple.conf;
+      environment.etc."drirc".text = ''
+        <?xml version="1.0" standalone="yes"?>
+        <!--
+
+        ============================================
+        Application bugs worked around in this file:
+        ============================================
+
+        * web broswer OpenGL renderer override fo asahi
+          Several web sites (paypal.com, etsy.com) apparently block on "Apple" in
+          WebGL renderer strings when the UA reports Linux as OS.
+          See https://github.com/webcompat/web-bugs/issues/189524
+
+        -->
+
+        <!DOCTYPE driconf [
+           <!ELEMENT driconf      (device+)>
+           <!ELEMENT device       (application | engine)+>
+           <!ATTLIST device       driver CDATA #IMPLIED
+                                  device CDATA #IMPLIED>
+           <!ELEMENT application  (option+)>
+           <!ATTLIST application  name CDATA #REQUIRED
+                                  executable CDATA #IMPLIED
+                                  executable_regexp CDATA #IMPLIED
+                                  sha1 CDATA #IMPLIED
+                                  application_name_match CDATA #IMPLIED
+                                  application_versions CDATA #IMPLIED>
+           <!ELEMENT engine       (option+)>
+
+           <!-- engine_name_match: A regexp matching the engine name -->
+           <!-- engine_versions: A version in range format
+                     (version 1 to 4 : "1:4") -->
+
+           <!ATTLIST engine       engine_name_match CDATA #REQUIRED
+                                  engine_versions CDATA #IMPLIED>
+
+           <!ELEMENT option       EMPTY>
+           <!ATTLIST option       name CDATA #REQUIRED
+                                  value CDATA #REQUIRED>
+        ]>
+
+        <driconf>
+            <device driver="asahi">
+      ''
+      + (lib.concatStringsSep "\n" (
+        lib.map (bin: ''
+          <application name="${bin}" executable="${bin}">
+             <option name="force_gl_renderer" value="AGX G1Nx"/>
+          </application>
+          <application name="${bin} Wrapped" executable=".${bin}-wrapped">
+             <option name="force_gl_renderer" value="AGX G1Nx"/>
+          </application>
+        '') cfg.webSecurityCompat
+      ))
+      + ''
+          </device>
+        </driconf>
+      '';
     };
 
   options.hardware.asahi = {
@@ -57,6 +114,34 @@
         System architecture that should be used to build the major Asahi
         packages, if not the default aarch64-linux. This allows installing from
         a cross-built ISO without rebuilding them during installation.
+      '';
+    };
+
+    webSecurityCompat = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "firefox"
+        "firefox-esr"
+        "firefox-beta"
+        "librewolf"
+        "chromium"
+        "chromium-browser"
+        "vivaldi-bin"
+        "iceweasel"
+        "epiphany"
+        "konqueror"
+        "falkon"
+        "waterfox"
+        "seamonkey"
+      ];
+      description = ''
+        Apply mesa workaround to specified browser binaries to avoid
+        false-positives on certain websites, including but not limited
+        to Paypal, Etsy and WSJ.
+
+        The default list contains a best-effort collection of
+        web-browsers, if a browser you use is not listed here, append
+        its browser binary here.
       '';
     };
 
