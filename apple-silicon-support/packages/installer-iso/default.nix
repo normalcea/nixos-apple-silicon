3596a549685e3e4a3f7b62b329975fd4f3ea5f1a
Jasi <jawsi@posteo.net>
2025-12-08 20:23:15 -0500

    default.nix: convert to using npins

---
+++ apple-silicon-support/packages/installer-iso/default.nix
@@ -0,0 +1,20 @@
+{
+  self,
+  system,
+  pkgs,
+}:
+(import (pkgs.path + "/nixos/lib/eval-config.nix") {
+  specialArgs = {
+    modulesPath = pkgs.path + "/nixos/modules";
+  };
+  modules = [
+    self.nixosModules.apple-silicon-support
+    ./installer-configuration.nix
+    {
+      hardware.asahi.pkgsSystem = system;
+      nixpkgs.hostPlatform.system = "aarch64-linux";
+      nixpkgs.buildPlatform.system = system;
+      nixpkgs.overlays = [ self.overlays.apple-silicon-overlay ];
+    }
+  ];
+}).config
